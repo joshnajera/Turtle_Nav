16005a7521b9ba6040b38c566b9445092be8d6bd
switch620 <switch620@gmail.com>
2017-10-26 17:22:14 -0700

    asdf

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/ap0c/Projects/Turtle_Nav/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ap0c/Projects/Turtle_Nav/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/ap0c/Projects/Turtle_Nav/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/ap0c/Projects/Turtle_Nav/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ap0c/Projects/Turtle_Nav/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ap0c/Projects/Turtle_Nav/src:$ROS_PACKAGE_PATH"
--- src/hw4/will_test.cpp
+++ src/hw4/will_test.cpp
@@ -4,6 +4,15 @@
 #include <sstream>
 #include <turtlesim/Spawn.h>
 #include <turtlesim/Kill.h>
+#include <string>
+#include <sstream>
+
+using namespace::std;
+
+void test(const turtlesim::Pose::ConstPtr& msg){
+	ROS_INFO("X: %f,  Y: %f", msg->x, msg->y);
+}
+
 int main(int argc, char **argv){
 
    ros::init(argc,argv,"hw4");
@@ -12,38 +21,37 @@ int main(int argc, char **argv){
 
    // <turtlesim::Spawn> <= Container type--  Has float32 x, float32 y, name
    // Topic: "spawn"
-   ros::ServiceClient client = n.serviceClient<turtlesim::Spawn>("spawn");
-
-/*
-   // Service object
-   turtlesim::Spawn srv;
-
-   srv.request.x= 0;
-   srv.request.y= 0;
-
-   if (client.call(srv)){
-   	ROS_INFO_STREAM(srv.response);
-   }
-
-   else{
-	   //ROS_ERROR("Failed to call");
-	   return 1;
-   }    
-*/
+   // ros::ServiceClient client = n.serviceClient<turtlesim::Spawn>("spawn");
 
 	turtlesim::Pose msg;	//  Defines the message obj
+/*
 	ros::Publisher turtle1pub = n.advertise<turtlesim::Pose>("/turtle1/pose", 100);
 	while(true){
-	ros::Rate loop_rate(2);
-	msg.x = 0.0;
-	msg.y = 0.0;
-	msg.theta = 0.0;
-	msg.linear_velocity = 0.0;
-	msg.angular_velocity = 0.0;
+		msg.x = 0.0;
+		msg.y = 0.0;
+		msg.linear_velocity = 1.0;
+		msg.angular_velocity = 0.0;
 
-	turtle1pub.publish(msg);
-	loop_rate.sleep();
-}
+		turtle1pub.publish(msg);
+		loop_rate.sleep();
+	}
+*/
+	stringstream tname;
+	
+	ros::Rate loop_rate(2);
+	// ros::Subscriber turtSub = n.subscribe<turtlesim::Pose>(tname.str(), 100, &test);
+
+	int turts = 1;
+	ros::Subscriber Tturts[turts];
+
+	for(int i = 0; i < turts; i ++ ){
+		tname.clear();
+		tname.str("");
+		tname << "/T" << i+1 << "/pose";
+		Tturts[i] = n.subscribe<turtlesim::Pose>(tname.str(), 100, &test);
+		loop_rate.sleep();
+		ros::spinOnce();
+	}
 
    return 0;
 }
